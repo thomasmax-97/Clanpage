5362e450b6d613fcf356cb8bbb5a737ab3746b71
Seiwix <manuelplaner@gmail.com>
2021-01-14 22:25:20 +0100

    Forntend/ work on Components

---
+++ components/Backgroundimag.vue
@@ -0,0 +1,10 @@
+<template>
+  
+  <div>
+  <img
+    class="bg-gradient-to-l md:bg-gradient-to-r w-full h-screen bg-center bg-no-repeat bg-cover"
+    src="~/assets/images/bg-texture-01.jpg"
+    img
+  />
+</div>
+</template>
