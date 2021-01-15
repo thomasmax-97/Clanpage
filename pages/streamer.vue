8e0594840ae6e9300431f9587fdebe85f497956a
Seiwix <manuelplaner@gmail.com>
2021-01-15 04:01:39 +0100

    page / steamer page add

---
+++ pages/streamer.vue
@@ -0,0 +1,50 @@
+<template>
+   <!-- component -->
+<div class="container my-12 mx-auto px-4 md:px-12">
+    <div class="flex flex-wrap -mx-1 lg:-mx-4">
+
+        
+        
+
+        <!-- Column -->
+        <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+
+            <!-- Article -->
+            <article class="overflow-hidden rounded-lg shadow-lg">
+
+                <a href="#">
+              <iframe src="https://player.twitch.tv/?channel=twitchpresents&parent=http://localhost:3000/&parent=http://localhost:3000/ts" 
+               height="220" 
+               width="380" 
+               frameborder="0" 
+               scrolling="no">
+              /></iframe>
+                </a>
+                <header class=" bg-gray-200 flex items-center justify-between leading-tight p-2 md:p-4">
+                    <h1 class="text-lg">
+                        <a class="no-underline hover:underline text-black" href="#">
+                            Steamer Name
+                        </a>
+                    </h1>
+                    
+                </header>
+
+                <footer class=" bg-gray-200 flex items-center justify-between leading-none p-2 md:p-4">
+                    <a class="flex items-center no-underline hover:underline text-black" href="#">
+                        <img alt="Placeholder" class="block rounded-full" src="https://picsum.photos/32/32/?random">
+                        <p class="ml-2 text-sm">
+                            Author Name
+                        </p>
+                    </a>
+                    <a class="no-underline text-grey-darker hover:text-red-dark" href="#">
+                        <span class="hidden">Like</span>
+                        <i class="fa fa-heart"></i>
+                    </a>
+                </footer>
+            </article>
+        </div>
+    </div>
+</div>
+
+</template>
+
