8fff9af53223480a937c522010f6bd32337adc18
Seiwix <manuelplaner@gmail.com>
2021-01-17 03:37:23 +0100

    frontend/add mamber page (new components) & revision index &footer

---
+++ components/Apply.vue
@@ -0,0 +1,43 @@
+<template>
+    <form id="contact-me" class="w-full mx-auto max-w-3xl bg-gray-500 shadow p-8 text-gray-700 ">
+    <h2 class="w-full my-2 text-3xl font-bold leading-tight my-5">Bewerben</h2>
+    <!-- name field -->
+    <div class="flex flex-wrap mb-6">
+        <div class="relative w-full appearance-none label-floating">
+            <input class="tracking-wide py-2 px-4 mb-3 leading-relaxed appearance-none block w-full bg-gray-200 border border-gray-200 rounded focus:outline-none focus:bg-white focus:border-gray-500"
+            id="name" type="text" placeholder="Name " required>
+            <label for="name" class="absolute tracking-wide py-2 px-4 mb-4 opacity-0 leading-tight block top-0 left-0 cursor-text">
+                Name
+            </label>
+        </div>
+    </div>
+    <!-- email field -->
+    <div class="flex flex-wrap mb-6">
+        <div class="relative w-full appearance-none label-floating">
+            <input class="tracking-wide py-2 px-4 mb-3 leading-relaxed appearance-none block w-full bg-gray-200 border border-gray-200 rounded focus:outline-none focus:bg-white focus:border-gray-500"
+            id="name" type="text" placeholder="Email" required>
+            <label for="name" class="absolute tracking-wide py-2 px-4 mb-4 opacity-0 leading-tight block top-0 left-0 cursor-text">
+                Email
+            </label>
+        </div>
+    </div>
+    <!-- Message field -->
+    <div class="flex flex-wrap mb-6">
+        <div class="relative w-full appearance-none label-floating">
+            <textarea class="autoexpand tracking-wide py-2 px-4 mb-3 leading-relaxed appearance-none block w-full bg-gray-200 border border-gray-200 rounded focus:outline-none focus:bg-white focus:border-gray-500"
+                id="message" type="text" placeholder="Message..."></textarea>
+                <label for="message" class="absolute tracking-wide py-2 px-4 mb-4 opacity-0 leading-tight block top-0 left-0 cursor-text">Message...
+            </label>
+        </div>
+    </div>
+
+    <div class="">
+        <button class="w-full shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+            type="submit">
+            Send
+        </button>
+
+    </div>
+</form>
+
+</template>
