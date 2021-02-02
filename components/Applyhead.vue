315a08f6311480bcd0f47f0db5d836ca83273c94
Maximilian Thomas <thompsenlp@googlemail.com>
2021-02-02 01:17:51 +0100

    rest: added fetch

--- components/Applyhead.vue
+++ components/Applyhead.vue
@@ -1,31 +1,38 @@
 <template>
-<div>
+  <div>
     <section class=" text-gray-200 ">
       <div class="max-w-6xl mx-auto px-5 py-24 ">
         <div class="flex flex-wrap w-full mb-20 flex-col items-center text-center">
-          <h1 class=" title-font mb-12 text-4xl font-extrabold leading-10 tracking-tight text-left sm:text-5xl sm:leading-none md:text-6xl"> Vereins Mitglied werden</h1>
-          
+          <h1 class=" title-font mb-12 text-4xl font-extrabold leading-10 tracking-tight text-left sm:text-5xl sm:leading-none md:text-6xl">
+            {{ data.heading }}
+          </h1>
           <p class="lg:w-1/2 w-full text-lg leading-relaxed ">
-           Du spielst gerne Computerspiele? Tauchst mit deinen Freunden, gemütlich im heimischen Sessel, oder mit den Kollegen in spannende Gameswelten ab? Du schaust gerne E-Sports Events in Arenen oder einfach vor dem heimischen TV und verfolgst deine lieblings E-Sports Team? 
+            {{ data.content }}
           </p>
         </div>
         <div class="flex flex-wrap -m-4">
           <div class="xl:w-1/3 md:w-1/2 p-4">
             <div class="border border-gray-300 bg-gray-900 p-6 rounded-lg">
               <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
-                <svg class=" fill-current h-6 w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M0 32l34.9 395.8L191.5 480l157.6-52.2L384 32H0zm308.2 127.9H124.4l4.1 49.4h175.6l-13.6 148.4-97.9 27v.3h-1.1l-98.7-27.3-6-75.8h47.7L138 320l53.5 14.5 53.7-14.5 6-62.2H84.3L71.5 112.2h241.1l-4.4 47.7z"/></svg>
+                <svg
+                  class=" fill-current h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 384 512"
+                ><path d="M0 32l34.9 395.8L191.5 480l157.6-52.2L384 32H0zm308.2 127.9H124.4l4.1 49.4h175.6l-13.6 148.4-97.9 27v.3h-1.1l-98.7-27.3-6-75.8h47.7L138 320l53.5 14.5 53.7-14.5 6-62.2H84.3L71.5 112.2h241.1l-4.4 47.7z" /></svg>
               </div>
-              <h2 class="text-lg  font-medium title-font mb-2">Community Regeln</h2>
-              <p class="leading-relaxed text-base"></p>
-              <a class = "underline" target="_blank" href="https://www.docdroid.net/lpVBmF9/vereins-mitgliedschaft-evodt-germany-playsgroup-pdf" 
+              <h2 class="text-lg  font-medium title-font mb-2">
+                Community Regeln
+              </h2>
+              <p class="leading-relaxed text-base" />
+              <a
+                class="underline"
+                target="_blank"
+                href="https://www.docdroid.net/lpVBmF9/vereins-mitgliedschaft-evodt-germany-playsgroup-pdf"
               >
-              Community Regeln (PDF)
+                Community Regeln (PDF)
               </a>
-              
-              <div class="text-center mt-2 leading-none flex justify-between w-full">
-                
-              </div>
 
+              <div class="text-center mt-2 leading-none flex justify-between w-full" />
             </div>
           </div>
 
@@ -36,39 +43,58 @@
                   Php
                 </span>
               </div>
-              <h2 class="text-lg  font-medium title-font mb-2">Vereins Satzung</h2>
-              <p class="leading-relaxed text-base"></p>
-              <a class = "underline" target="_blank" href="https://www.docdroid.net/lpVBmF9/vereins-mitgliedschaft-evodt-germany-playsgroup-pdf" 
+              <h2 class="text-lg  font-medium title-font mb-2">
+                Vereins Satzung
+              </h2>
+              <p class="leading-relaxed text-base" />
+              <a
+                class="underline"
+                target="_blank"
+                href="https://www.docdroid.net/lpVBmF9/vereins-mitgliedschaft-evodt-germany-playsgroup-pdf"
               >
-              Zur Satzung (PDF)
+                Zur Satzung (PDF)
               </a>
-              
-              <div class="text-center mt-2 leading-none flex justify-between w-full">
-                
-              </div>
+
+              <div class="text-center mt-2 leading-none flex justify-between w-full" />
             </div>
           </div>
           <div class="xl:w-1/3 md:w-1/2 p-4">
             <div class="border border-gray-300 bg-gray-900  p-6 rounded-lg">
               <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
-                <svg class=" fill-current h-6 w-6 " viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>CSS3 icon</title><path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.565-2.438L1.5 0zm17.09 4.413L5.41 4.41l.213 2.622 10.125.002-.255 2.716h-6.64l.24 2.573h6.182l-.366 3.523-2.91.804-2.956-.81-.188-2.11h-2.61l.29 3.855L12 19.288l5.373-1.53L18.59 4.414z"/></svg>
+                <svg
+                  class=" fill-current h-6 w-6 "
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                ><title>CSS3 icon</title><path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.565-2.438L1.5 0zm17.09 4.413L5.41 4.41l.213 2.622 10.125.002-.255 2.716h-6.64l.24 2.573h6.182l-.366 3.523-2.91.804-2.956-.81-.188-2.11h-2.61l.29 3.855L12 19.288l5.373-1.53L18.59 4.414z" /></svg>
               </div>
-              <h2 class="text-lg  font-medium title-font mb-2">Vereinsbeitrag</h2>
-              <p class="leading-relaxed text-base"></p>
-              <a class = "underline" target="_blank" href="https://www.docdroid.net/lpVBmF9/vereins-mitgliedschaft-evodt-germany-playsgroup-pdf" 
+              <h2 class="text-lg  font-medium title-font mb-2">
+                Vereinsbeitrag
+              </h2>
+              <p class="leading-relaxed text-base" />
+              <a
+                class="underline"
+                target="_blank"
+                href="https://www.docdroid.net/lpVBmF9/vereins-mitgliedschaft-evodt-germany-playsgroup-pdf"
               >
-              Community Regeln (PDF)
+                Community Regeln (PDF)
               </a>
-              
-              <div class="text-center mt-2 leading-none flex justify-between w-full">
-                
-              </div>
 
+              <div class="text-center mt-2 leading-none flex justify-between w-full" />
             </div>
           </div>
         </div>
       </div>
     </section>
-
-</div>
+  </div>
 </template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Object,
+      required: true,
+    }
+  },
+}
+</script>
--- components/StreamCard.vue
+++ components/StreamCard.vue
@@ -3,23 +3,33 @@
     <div class="h-full border-2 border-gray-800 bg-gray-900 rounded-lg overflow-hidden">
       <img
         class="lg:h-48 md:h-36 w-full object-cover object-center"
-        src="https://dummyimage.com/720x400"
+        :src="data.image"
         alt="blog"
       >
       <div class="p-6">
         <h1 class="title-font text-lg font-medium text-white mb-3">
-        <a  href="https://www.twitch.tv/" target="_blank">
-        Streamer Name
-        </a>
-         </h1>
+          <a href="https://www.twitch.tv/" target="_blank">
+            {{data.players}}
+          </a>
+        </h1>x
         <p class="leading-relaxed mb-3">
-            Streamer Text Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.
+          {{data.desc}}
         </p>
         <div class="flex items-center flex-wrap ">
-          
-        
         </div>
       </div>
     </div>
   </div>
 </template>
+
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Object,
+      required: true,
+    }
+  },
+}
+</script>
