57b5d3e07323ebbc1cb3ae08346edf2329ad5331
Maximilian Thomas <thompsenlp@googlemail.com>
2021-02-05 07:35:14 +0100

    config update

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,7 +37,7 @@ export default {
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {
-    baseURL: process.env.BASE_URL
+    //baseURL: process.env.BASE_URL
   },
 
   publicRuntimeConfig: {
